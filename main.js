03832c908a28d0cb1be4418ab8adadbd1cbb46a2
Kaikai <kailin.gao@gmail.com>
2018-02-03 22:19:58 -0800

    Food Gallery

---
+++ main.js
@@ -0,0 +1,31 @@
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+
+// Set first image opacity
+imgs[0].style.opacity = opacity;
+
+// Destructuring
+// const [current, imgs] = [
+//   document.querySelector("#current"),
+//   document.querySelectorAll(".imgs img")
+// ];
+
+imgs.forEach(img => img.addEventListener("click", imgClick));
+
+function imgClick(e) {
+  // Reset the opacity
+  imgs.forEach(img => (img.style.opacity = 1));
+
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+
+  // Add fade in class
+  current.classList.add("fade-in");
+
+  // Remove fade-in class after 0.5s
+  setTimeout(() => current.classList.remove("fade-in"), 500);
+
+  // Change opacity to opacity var
+  e.target.style.opacity = opacity;
+}
